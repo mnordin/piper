b47086a66ed4a8ee950535e39e75c95d4b0aaaa3
Markus Nordin <markus.nordin@gmail.com>
2009-05-14 14:05:50 +0200

    Fix for vendors, self contained

--- config/environment.rb
+++ config/environment.rb
@@ -8,9 +8,10 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
 
-  config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+  config.frameworks -= [ :active_support, :active_record, :active_resource, :action_mailer ]
 
   config.gem "httparty"
+  config.gem "crack"
 
   config.time_zone = 'UTC'
   
